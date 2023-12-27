545203029abd512d7cba35b74dfe6c1aaf5ce0a1
Boki-Bok <154994479+Boki-Bok@users.noreply.github.com>
2023-12-27 19:48:25 +0100

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -9,17 +9,15 @@ loginButton.addEventListener("click", (e) => {
     // Get the values input by the user in the form fields
     const username = loginForm.username.value;
     const password = loginForm.password.value;
-    const logged_in = 0;
 
     if (username === "itas" && password === "bokidomain") {
         // If the credentials are valid, show a welcome message and a pict
         document.write('<p style="font-size:25px;color:green;">Welcome '+username+' !</p>')
-	document.write('<img src="apache.jpg" alt="APA_FORM" >')
+		document.write('<img src="apache.jpg" alt="APA_FORM" >')
 
     } else {
         // Otherwise, make the login error message show (change its oppacity)
         loginErrorMsg.style.opacity = 1;
     }
 })
-
 
