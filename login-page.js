b29b336b2548176ec351b6a8dd871115050fd7ea
Boki-Bok <154994479+Boki-Bok@users.noreply.github.com>
2023-12-28 10:09:43 +0100

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -13,17 +13,22 @@ loginButton.addEventListener("click", (e) => {
     if (username === "itas" && password === "bokidomain") {
         // If the credentials are valid, show a welcome message and a pict
         document.write('<p style="font-size:25px;color:green;">Welcome '+username+' !</p>')
-		document.write('<img src="apache.jpg" alt="APA_FORM" >')
+	document.write('<img src="apache.jpg" alt="APA_FORM" >')
 
-		// set a small cookie session 
-		let date = new Date(Date.now() + 60000);
-		date = date.toUTCString();
-		document.cookie = 'user = itas; path = /; domain = bokidomain; expires = ' + date + '' ;
+	// set a small cookie session 
+	let date = Date.now() + 60000;
+	date = date.toUTCString();
+	var sessionObject = {
+		expiresAt: date,
+		someOtherSessionData: {
+			"username": "itas",
+			"app" : "bokidomain"
+		}
+	}
+	sessionStorage.setItem('sessionObject', JSON.stringify(sessionObject));
 
     } else {
         // Otherwise, make the login error message show (change its oppacity)
         loginErrorMsg.style.opacity = 1;
     }
-	
-
 })
